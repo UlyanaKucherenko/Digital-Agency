ee48ed9fb803356a1dde6900f7c98270199e3f18
Uliana Kucherenko <ulya@Ulianas-MacBook-Air.local>
2023-09-23 16:57:10 +0300

    fix: made changes for mr

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,8 @@
 import logo from '~/assets/img/logo.svg';
-import { ListLine } from '~/components/common/ListLine';
 import { FooterListMenu, ListSocialNetwork } from '~/utils/const';
+import { ListLine } from '~/components/common/ListLine';
+import { FooterLinksColumn } from './FooterLinksColumn';
+import { UsefulLinks, HitUsUpList } from './config';
 import styles from './index.module.css';
 
 export const Footer = () => {
@@ -21,40 +23,12 @@ export const Footer = () => {
           <div className={styles.footerInfoColumn}>
             <div>
               <h5 className={styles.footerInfoTitle}>Address:</h5>
-              <div className={styles.footer__infoWrapText}>
-                <p className={styles.footer__infoText}>27 Division St, New York, NY 10002, USA</p>
-              </div>
-            </div>
-            <div>
-              <h5 className={styles.footerInfoTitle}>Hit us up:</h5>
               <div className={styles.footerInfoWrapText}>
-                <p className={styles.footerInfoText}>
-                  <a className={styles.footerInfoTextLink} href='tel:+1 344 789 07 68'>
-                    +1 344 789 07 68
-                  </a>
-                </p>
-                <p className={styles.footerInfoText}>
-                  <a className={styles.footerInfoTextLink} href='mailto:info@sitename.com'>
-                    info@sitename.com
-                  </a>
-                </p>
+                <p className={styles.footerInfoText}>27 Division St, New York, NY 10002, USA</p>
               </div>
             </div>
-            <div>
-              <h5 className={styles.footerInfoTitle}>Useful links</h5>
-              <div className={styles.footerInfoWrapText}>
-                <p className={styles.footerInfoText}>
-                  <a className={styles.footerInfoTextLink} href='/'>
-                    Help and Support
-                  </a>
-                </p>
-                <p className={styles.footerInfoText}>
-                  <a className={styles.footerInfoTextLink} href='/'>
-                    Careers
-                  </a>
-                </p>
-              </div>
-            </div>
+            <FooterLinksColumn title='Hit us up:' list={HitUsUpList} />
+            <FooterLinksColumn title='Useful links' list={UsefulLinks} />
           </div>
         </div>
       </div>
