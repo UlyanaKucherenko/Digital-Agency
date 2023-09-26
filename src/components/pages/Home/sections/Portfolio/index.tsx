b784b7373c8129e4e48e1142378e15c655707a6e
Uliana Kucherenko <ulya@Ulianas-MacBook-Air.local>
2023-09-26 12:52:11 +0300

    feat: added section Portfolio

---
+++ src/components/pages/Home/sections/Portfolio/index.tsx
@@ -0,0 +1,38 @@
+import styles from './index.module.css';
+import img1 from '~/assets/img/pages/home/portfolio/1.jpg';
+import img2 from '~/assets/img/pages/home/portfolio/2.jpg';
+import img3 from '~/assets/img/pages/home/portfolio/3.jpg';
+import { TitleSection } from '~/components/common/TitleSection';
+import { VerticalTitle } from '~/components/common/VerticalTitle';
+
+export const Portfolio = () => {
+  return (
+    <section className={styles.sectionPortfolio}>
+      <div className={styles.wrapper}>
+        <div className={styles.wrapperTitle}>
+          <TitleSection
+            title='check our latest cases'
+            subtitle='portfolio'
+            linkPath='/'
+            textLink='view all'
+            inverted
+          />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.list}>
+            <div className={styles.listItem}>
+              <img src={img1} alt='' />
+            </div>
+            <div>
+              <img src={img2} alt='' />
+            </div>
+            <div>
+              <img src={img3} alt='' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <VerticalTitle bigTitle='cases' title='cases' color='var(--blackHole)' />
+    </section>
+  );
+};
